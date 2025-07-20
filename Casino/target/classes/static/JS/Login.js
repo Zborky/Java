b0992b0f46e24969de33022232ca72b2af706eca
Zborky <jakubzboron11@gmail.com>
2025-07-20 21:34:29 +0200

    Delete Casino directory

--- Casino/target/classes/static/JS/Login.js
+++
@@ -1,6 +0,0 @@
-if (window.location.search.includes('error')) {
-                    document.write('Nesprávne prihlasovacie údaje.');
-                }
-                if (window.location.search.includes('logout')) {
-                    document.write('Úspešne ste sa odhlásili.');
-                }
